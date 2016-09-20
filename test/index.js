0cdcea1b013b790ff56ac3e6c0b0d229d35f0722
Shiv arora <aroras@cromwell.co.uk>
2016-09-20 21:24:21 +0100

    amended

--- test/index.js
+++ test/index.js
@@ -2,26 +2,43 @@ import fs from 'fs';
 import async from 'async';
 import buildFeed from '../src/feed';
 import YoutubePodcast from '../src/index';
+import config  from '../src/config.json';
+import nodeSchedule from "node-schedule";
+import {SendMail} from "../src/lib/sendMail";
 
-let yp = new YoutubePodcast(
-{
-	buildURLFunction: (videoId) => 'http://podsync.net/download/youtube/' + videoId ,
-	apiKey: 'AIzaSyAcIv3VxVE1n61pW-WrN5iTbW8yfkx-5TY',
-	maxVideos: 30,
-	cacheTTL: 1800
-});
+let sendMail = new SendMail();
 
-yp.feedForChannel('UCdiSVEfN3qg0VeZO3t4vueA', function(err, xml)
-{
-	console.log(err || xml);
+nodeSchedule.scheduleJob(config.runTime, function () {
+    let yp = new YoutubePodcast(
+        {
+            buildURLFunction: (videoId) => 'http://podsync.net/download/youtube/' + videoId,
+            apiKey: 'AIzaSyAcIv3VxVE1n61pW-WrN5iTbW8yfkx-5TY',
+            maxVideos: 30,
+            cacheTTL: 1800
+        });
 
-fs.writeFile( __dirname + "/../output.xml", xml, function(err) {
+    yp.feedForChannel('UCdiSVEfN3qg0VeZO3t4vueA', function (err, xml) {
+        console.log(err || xml);
 
-    if(err) {
-        return console.log(err);
-    }
+        fs.writeFile(__dirname + "/../output.xml", xml, function (err) {
 
-    console.log("The file was saved!");
-});
-   // process.exit();
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+
+            // setup e-mail data with unicode symbols
+            var mailOptions = {
+                from: "Pep Talk<peptalkukpodcast@gmail.com>", // sender address
+                to: "shibi.arora@gmail.com", // list of receivers
+                subject: "Podcast: Podcast status", // Subject line
+                text: 'Podcast running successfully ', // plaintext body
+            };
+
+            console.log("sending mail");
+            sendMail.mail(mailOptions);
+        });
+        // process.exit();
+    });
 });
